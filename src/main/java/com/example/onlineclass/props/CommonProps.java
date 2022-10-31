3d60af8fc27541dbf861fcf792f38e9f74f3b7a0
jhlyh <76247576+jhlyh@users.noreply.github.com>
2022-10-31 15:02:59 +0800

    Update CommonProps.java

--- src/main/java/com/example/onlineclass/props/CommonProps.java
+++ src/main/java/com/example/onlineclass/props/CommonProps.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class CommonProps {
-    private String ftpPort = "21";
-    private String ftpHost = "114.132.251.197";
-    private String ftpUserName = "root";
-    private String ftpPassword = "chCZD5hNS4wH7tL2";
+    private String ftpPort = "**";
+    private String ftpHost = "**";
+    private String ftpUserName = "**";
+    private String ftpPassword = "**";
     private String ftpImagePathName = "/images";
     private String ftpVideoPathName = "/videos";
-    private String nginxPath = "http://114.132.251.197:888/";
+    private String nginxPath = "**";
     private String frontEndError = "4xx";
     private String afterEndError = "5xx";
 
